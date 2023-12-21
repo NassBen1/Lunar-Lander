ebe0d12502e1b213ec4ac872ac95f1e2860daa4b
NassBen1 <ben.nassim@icloud.com>
2023-12-21 23:37:01 +0100

    Démarrage et import de Tensorflow

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import gymnasium as gym
+import numpy as np
+
+import time
+from collections import deque, namedtuple # for replay memory
+import random
+import PIL.Image
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MSE
+
+#Valeur à changer pour le nombre d'itération
+tf.random.set_seed(42)
+
+# Hyperparameters
+memorysize = 1000000
+gamma = 0.99
+alpha = 0.0001
+numbers_steps_for_update = 4
+
+
 env = gym.make("LunarLander-v2", render_mode="human")
-observation, info = env.reset()
+
+state_size = env.observation_space.shape
+action_size = env.action_space.n
+
+current_state = env.reset()
+
+print("State size:", state_size)
+print("Action size:", action_size)
 
 for _ in range(1000):
     action = env.action_space.sample()  # agent policy that uses the observation and info
