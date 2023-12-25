89727c89448c73f371bf57621dc89ae53294f625
LABBI <mohamed.labbi@cgi.com>
2023-12-25 19:08:07 +0100

    création réseau de Neurone avec Torch

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+"""Création de l'architecture du réseau neuronal"""
+
+class ReseauNeuronal(nn.Module):
+    def __init__(self, taille_etat, taille_action, seed=42):
+        super(ReseauNeuronal, self).__init__()
+        self.graine = torch.manual_seed(seed)
+        self.couche_connectee_1 = nn.Linear(taille_etat, 64)
+        self.couche_connectee_2 = nn.Linear(64, 64)
+        self.couche_connectee_3 = nn.Linear(64, taille_action)
+
+    def forward(self, etat):
+        x = self.couche_connectee_1(etat)
+        x = F.relu(x)
+        x = self.couche_connectee_2(x)
+        x = F.relu(x)
+        return self.couche_connectee_3(x)
+
 import gymnasium as gym
+
 env = gym.make("LunarLander-v2", render_mode="human")
 observation, info = env.reset()
 
