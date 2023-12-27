66213cdf3141921a04ab2a04757a096745aae4d9
LABBI <mohamed.labbi@cgi.com>
2023-12-27 22:30:05 +0100

    import numpy and random

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import random
+import numpy as np
 import torch.nn.functional as F
 
 """Création de l'architecture du réseau neuronal"""
