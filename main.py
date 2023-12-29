103ea36e4e90946db30ecf12c13b54cdee59be5e
Nassim Benhamou <nassim.benhamou@etu.imt-nord-europe.fr>
2023-12-29 18:33:51 +0100

    Finalisation Classe Agent et Initialisation de l'agent

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import random
 import numpy as np
 import torch.nn.functional as F
 
+from collections import deque
+
 """Création de l'architecture du réseau neuronal"""
+
 
 class ReseauNeuronal(nn.Module):
     def __init__(self, taille_etat, taille_action, seed=42):
@@ -22,9 +25,11 @@ class ReseauNeuronal(nn.Module):
         x = F.relu(x)
         return self.couche_connectee_3(x)
 
-#Mise en place de l'environnement et récupération des informations sur l'état et les actions
+
+# Mise en place de l'environnement et récupération des informations sur l'état et les actions
 
 import gym
+
 env = gym.make("LunarLander-v2")
 forme_etat = env.observation_space.shape
 print("Forme de l'état =", forme_etat)
@@ -33,7 +38,7 @@ print("Taille de l'état =", taille_etat)
 nb_actions = env.action_space.n
 print("Nombre d'actions =", nb_actions)
 
-#Initialisation des hyperparamètres
+# Initialisation des hyperparamètres
 
 taux_apprentissage = 5e-4
 taille_mini_batch = 100
@@ -41,19 +46,19 @@ facteur_discount = 0.98
 taille_memoire_replay = int(1e5)
 parametre_interpolation = 1e-3
 
-#Mise en place du replay memory
+
+# Mise en place du replay memory
 
 class MemoireReplay(object):
-
-
-
     """Initialisation du replay memory"""
+
     def __init__(self, capacite):
         self.appareil = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.capacite = capacite
         self.memoire = []
 
     """Ajout d'un événement dans le replay memory"""
+
     def push(self, evenement):
         self.memoire.append(evenement)
         if len(self.memoire) > self.capacite:
@@ -63,12 +68,17 @@ class MemoireReplay(object):
         experiences = random.sample(self.memoire, k=taille_batch)
         etats = torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None])).float().to(self.appareil)
         actions = torch.from_numpy(np.vstack([e[1] for e in experiences if e is not None])).long().to(self.appareil)
-        recompenses = torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None])).float().to(self.appareil)
-        etats_suivants = torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None])).float().to(self.appareil)
-        termines = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(self.appareil)
+        recompenses = torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None])).float().to(
+            self.appareil)
+        etats_suivants = torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None])).float().to(
+            self.appareil)
+        termines = torch.from_numpy(
+            np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(self.appareil)
         return etats, etats_suivants, actions, recompenses, termines
 
+
 """Implémentation de la classe DQN"""
+
 
 class Agent():
 
@@ -82,3 +92,73 @@ class Agent():
         self.memoire = MemoireReplay(taille_memoire_replay)
         self.etapes_temps = 0
 
+    def etape(self, etat, action, recompense, etat_suivant, termine):
+        self.memoire.push((etat, action, recompense, etat_suivant, termine))
+        self.etapes_temps = (self.etapes_temps + 1) % 4
+        if self.etapes_temps == 0:
+            if len(self.memoire.memoire) > taille_mini_batch:
+                experiences = self.memoire.echantillon(100)
+                self.apprendre(experiences, facteur_discount)
+
+    def agir(self, etat, epsilon=0.):
+        etat = torch.from_numpy(etat).float().unsqueeze(0).to(self.appareil)
+        self.reseau_local_Q.eval()
+        with torch.no_grad():
+            valeurs_action = self.reseau_local_Q(etat)
+        self.reseau_local_Q.train()
+
+        if random.random() > epsilon:
+            return np.argmax(valeurs_action.cpu().data.numpy())
+        else:
+            return random.randint(0, self.taille_action - 1)
+
+    def apprendre(self, experiences, facteur_discount):
+        etats, etats_suivants, actions, recompenses, termines = experiences
+        prochaines_valeurs_Q_cibles = self.reseau_cible_Q(etats_suivants).detach().max(1)[0].unsqueeze(1)
+        valeurs_Q_cibles = recompenses + (facteur_discount * prochaines_valeurs_Q_cibles * (1 - termines))
+        valeurs_Q_attendues = self.reseau_local_Q(etats).gather(1, actions)
+        perte = F.mse_loss(valeurs_Q_attendues, valeurs_Q_cibles)
+        self.optimiseur.zero_grad()
+        perte.backward()
+        self.optimiseur.step()
+        self.mise_a_jour_progressive(self.reseau_local_Q, self.reseau_cible_Q, parametre_interpolation)
+
+    def mise_a_jour_progressive(self, modele_local, modele_cible, parametre_interpolation):
+        for parametre_cible, parametre_local in zip(modele_cible.parameters(), modele_local.parameters()):
+            parametre_cible.data.copy_(parametre_interpolation * parametre_local.data + (
+                    1.0 - parametre_interpolation) * parametre_cible.data)
+
+    """Initialisation de l'Agent DQN"""
+
+
+agent = Agent(taille_etat, nb_actions)
+
+num_episodes = 500
+max_num_time_steps = 1000
+epsilon_debut = 1.0
+epsilon_fin = 0.01
+taux_de_decay_epsilon = 0.995
+epsilon = epsilon_debut
+scores_sur_100_episodes = deque(maxlen=100)
+
+for episode in range(1, num_episodes + 1):
+    etat, _ = env.reset()
+    score = 0
+    for pas_temporel in range(max_num_time_steps):
+        action = agent.agir(etat, epsilon)
+        etat_suivant, recompense, termine, _, _ = env.step(action)
+        agent.etape(etat, action, recompense, etat_suivant, termine)
+        etat = etat_suivant
+        score += recompense
+        if termine:
+            break
+    scores_sur_100_episodes.append(score)
+    epsilon = max(epsilon_fin, taux_de_decay_epsilon * epsilon)
+    print('\rEpisode {}\tScore Moyen: {:.2f}'.format(episode, np.mean(scores_sur_100_episodes)), end="")
+    if episode % 100 == 0:
+        print('\rEpisode {}\tScore Moyen: {:.2f}'.format(episode, np.mean(scores_sur_100_episodes)))
+    if np.mean(scores_sur_100_episodes) >= 200.0:
+        print('\rEnvironnement résolu en {:d}\tScore Moyen: {:.2f}'.format(episode - 100,
+                                                                           np.mean(scores_sur_100_episodes)))
+        torch.save(agent.reseau_local_Q.state_dict(), 'checkpoint.pth')
+        break
